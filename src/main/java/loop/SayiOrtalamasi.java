b7853ae8c19c708457310cbf1266cef50add0632
Ferhat Bayturk <bayfbfb@gmail.com>
2023-01-28 19:15:54 +0300

    patika

--- src/main/java/loop/SayiOrtalamasi.java
+++ src/main/java/loop/SayiOrtalamasi.java
@@ -3,6 +3,10 @@ package loop;
 import java.util.Scanner;
 
 public class SayiOrtalamasi {
+    /*\
+    Java döngüler ile 0'dan girilen sayıya kadar olan sayılardan
+     3 ve 4'e tam bölünen sayıların ortalamasını hesaplayan programı yazınız.
+     */
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
@@ -12,7 +16,7 @@ public class SayiOrtalamasi {
             double sum =0;
             double sayac =0;
 
-        for (int i = 0; i < sayi; i++) {
+        for (int i = 0; i <= sayi; i++) {
 
             if(i%3==0 && i%4==0){
                 sayac++;
